d1ca352a649cd0c212ee5a6927fe58409feb1420
mishgan_ische <arzmisha@gmail.com>
2026-02-26 18:51:49 +0100

    Refactoring of Character

--- Source/MyProject/Private/TestCharacter.cpp
+++ Source/MyProject/Private/TestCharacter.cpp
@@ -1,196 +1,92 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Shoot Them Up Game, All Rights Reserved.
 
 #include "TestCharacter.h"
-
-#include "Components/STUWeaponComponent.h"
-#include "Components/TextRenderComponent.h"
-#include "Components/InputComponent.h"
-#include "Engine/EngineTypes.h"
-#include "Engine/DamageEvents.h"
-#include "GameFramework/SpringArmComponent.h"
-#include "Camera/CameraComponent.h"
 #include "STUCharacterMovementComponent.h"
-#include "GameFramework/Controller.h"
-#include "Components/CapsuleComponent.h"
 #include "STUHealthComponent.h"
+#include "Components/STUWeaponComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Engine/DamageEvents.h"
+#include "GameFramework/Controller.h"
 
-DEFINE_LOG_CATEGORY_STATIC(BaseCharacterLog,All,All);
+DEFINE_LOG_CATEGORY_STATIC(LogBaseCharacter, All, All);
 
-
-// Sets default values
-ATestCharacter::ATestCharacter(const FObjectInitializer& ObjInit):Super (ObjInit.SetDefaultSubobjectClass<USTUCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
+ATestCharacter::ATestCharacter(const FObjectInitializer& ObjInit)
+    : Super(ObjInit.SetDefaultSubobjectClass<USTUCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
 
-	SpringArmComponent=CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
-	SpringArmComponent->SetupAttachment(GetRootComponent());
-	SpringArmComponent->bUsePawnControlRotation=true;
-	SpringArmComponent->SocketOffset=FVector(0.0f,100.0f,80.0f);
-
-	
-	CameraComponent= CreateDefaultSubobject<UCameraComponent>("CameraComponent");
-	CameraComponent->SetupAttachment(SpringArmComponent);
-
-	
-	STUHealthComponent = CreateDefaultSubobject<USTUHealthComponent>("HealthComponent");
-
-
-	TextRenderComponent = CreateDefaultSubobject<UTextRenderComponent>("TextRenderComponent");
-	TextRenderComponent->SetupAttachment(GetRootComponent());
-	TextRenderComponent->SetOwnerNoSee(true);
-
-	WeaponComponent = CreateDefaultSubobject<USTUWeaponComponent>("WeaponComponent");
+    HealthComponent = CreateDefaultSubobject<USTUHealthComponent>("HealthComponent");
+    WeaponComponent = CreateDefaultSubobject<USTUWeaponComponent>("WeaponComponent");
 }
 
-// Called when the game starts or when spawned
 void ATestCharacter::BeginPlay()
 {
-	Super::BeginPlay();
-	check(STUHealthComponent);
-	check(TextRenderComponent);
-	check(GetCharacterMovement());
-	check(GetMesh());
+    Super::BeginPlay();
 
-	OnHealthChanged(STUHealthComponent->GetHealth(), 0.0f);
-	STUHealthComponent->OnDeath.AddUObject(this, &ATestCharacter::OnDeath);
-	STUHealthComponent->OnHealthChanged.AddUObject(this, &ATestCharacter::OnHealthChanged);
+    check(HealthComponent);
+    check(GetCharacterMovement());
+    check(GetCapsuleComponent());
+    check(GetMesh());
 
-	LandedDelegate.AddDynamic(this,&ATestCharacter::OnGroundLanded);
-	
+    OnHealthChanged(HealthComponent->GetHealth(), 0.0f);
+    HealthComponent->OnDeath.AddUObject(this, &ATestCharacter::OnDeath);
+    HealthComponent->OnHealthChanged.AddUObject(this, &ATestCharacter::OnHealthChanged);
+
+    LandedDelegate.AddDynamic(this, &ATestCharacter::OnGroundLanded);
 }
 
+void ATestCharacter::OnHealthChanged(float Health, float HealthDelta) {}
 
-// Called every frame
 void ATestCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
-	//TakeDamage(0.1f, FDamageEvent {}, Controller, this);
+    Super::Tick(DeltaTime);
 }
-
-
-
-// Called to bind functionality to input
-void ATestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	check(PlayerInputComponent);
-	check(WeaponComponent)
-	
-	PlayerInputComponent->BindAxis("MoveForward", this, &ATestCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ATestCharacter::MoveRight);
-	PlayerInputComponent->BindAxis("LookUp", this, &ATestCharacter::LookUp);
-	PlayerInputComponent->BindAxis("TurnAround", this, &ATestCharacter::TurnRight);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ATestCharacter::Jump);
-	PlayerInputComponent->BindAction("Run", IE_Pressed, this, &ATestCharacter::OnFastRunning);
-	PlayerInputComponent->BindAction("Run", IE_Released, this, &ATestCharacter::OnStopRunning);
-	PlayerInputComponent->BindAction("Fire", IE_Pressed, WeaponComponent, &USTUWeaponComponent::StartFire);
-	PlayerInputComponent->BindAction("Fire", IE_Released, WeaponComponent, &USTUWeaponComponent::StopFire);
-	PlayerInputComponent->BindAction("NextWeapon", IE_Released, WeaponComponent, &USTUWeaponComponent::NextWeapon);
-	PlayerInputComponent->BindAction("PreviousWeapon", IE_Released, WeaponComponent, &USTUWeaponComponent::PreviousWeapon);
-	PlayerInputComponent->BindAction("Reload", IE_Released, WeaponComponent, &USTUWeaponComponent::Reload);
-}
-
-void ATestCharacter::MoveForward(float Value)
-{
-	IsMoving = Value> 0.0f;
-	if (Value==0.0f) return;
-	AddMovementInput(GetActorForwardVector(), Value);
-}
-
-void ATestCharacter::MoveRight(float Value)
-{
-	if (Value==0.0f) return;
-	AddMovementInput(GetActorRightVector(),Value);
-}
-
-void ATestCharacter::LookUp(float Value)
-{
-	AddControllerPitchInput(Value);
-}
-
-void ATestCharacter::TurnRight(float Value)
-{
-	AddControllerYawInput(Value);
-}
-
-
-void ATestCharacter::OnFastRunning()
-{
-	WantsToRun=true;
-}
-
-void ATestCharacter::OnStopRunning()
-{
-	WantsToRun=false;
-}
-
 
 bool ATestCharacter::IsRunning() const
 {
-	return WantsToRun && IsMoving && !GetVelocity().IsZero();
+    return false;
 }
 
-float ATestCharacter::GetActorDirection() const
+float ATestCharacter::GetMovementDirection() const
 {
-	if (GetVelocity().IsZero()) return 0.0f;
-	const auto VelocityNormal = GetVelocity().GetSafeNormal();
-	const auto AngleBetween = FMath::Acos(FVector::DotProduct(VelocityNormal,GetActorForwardVector()));
-	const auto CrossProduct = FVector::CrossProduct(VelocityNormal,GetActorForwardVector());
-	const auto Degrees = FMath::RadiansToDegrees(AngleBetween);
-	return CrossProduct.IsZero() ? Degrees : Degrees* FMath::Sign(CrossProduct.Z);
+    if (GetVelocity().IsZero()) return 0.0f;
+    const auto VelocityNormal = GetVelocity().GetSafeNormal();
+    const auto AngleBetween = FMath::Acos(FVector::DotProduct(GetActorForwardVector(), VelocityNormal));
+    const auto CrossProduct = FVector::CrossProduct(GetActorForwardVector(), VelocityNormal);
+    const auto Degrees = FMath::RadiansToDegrees(AngleBetween);
+    return CrossProduct.IsZero() ? Degrees : Degrees * FMath::Sign(CrossProduct.Z);
 }
-
-void ATestCharacter::SetPlayerColor(const FLinearColor Color)
-{
-	const auto MaterialInst = GetMesh()->CreateAndSetMaterialInstanceDynamic(0);
-	if (!MaterialInst) return;
-	
-	MaterialInst->SetVectorParameterValue(MaterialColorName, Color);
-}
-
 
 void ATestCharacter::OnDeath()
 {
-	UE_LOG(BaseCharacterLog, Display, TEXT("Player %s is dead!"), *GetName());
+    UE_LOG(LogBaseCharacter, Display, TEXT("Player %s is dead"), *GetName());
 
-	//PlayAnimMontage(DeathAnimMontage);
-	GetCharacterMovement()->DisableMovement();
-	SetLifeSpan(10.0f);
+    // PlayAnimMontage(DeathAnimMontage);
+    GetCharacterMovement()->DisableMovement();
+    SetLifeSpan(LifeSpanOnDeath);
 
-	Controller->ChangeState(NAME_Spectating);
+    GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+    WeaponComponent->StopFire();
 
-	GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
-	WeaponComponent->StopFire();
-	GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	GetMesh()->SetSimulatePhysics(true);
-}
-
-void ATestCharacter::OnHealthChanged(float Health, float HealthDelta)
-{
-	TextRenderComponent->SetText(FText::FromString(FString::Printf(TEXT("%.0f"), Health)));
+    GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    GetMesh()->SetSimulatePhysics(true);
 }
 
 void ATestCharacter::OnGroundLanded(const FHitResult& Hit)
 {
-	const auto FallVelocityZ = -GetVelocity().Z;
+    const auto FallVelocityZ = -GetVelocity().Z;
+    if (FallVelocityZ < LandedDamageVelocity.X) return;
 
-	if (FallVelocityZ < VelocityLandedDamage.X) return;
+    const auto FallDamage = FMath::GetMappedRangeValueClamped(LandedDamageVelocity, LandedDamage, FallVelocityZ);
+    TakeDamage(FallDamage, FDamageEvent{}, nullptr, nullptr);
 
-	const auto FinalDamage = FMath::GetMappedRangeValueClamped(VelocityLandedDamage, LandedDamage, FallVelocityZ);
-
-	FDamageEvent DamageEvent;
-	TakeDamage(FinalDamage, DamageEvent, nullptr, nullptr);
-	
+    UE_LOG(LogBaseCharacter, Display, TEXT("Player %s recived landed damage: %f"), *GetName(), FallDamage);
 }
 
+void ATestCharacter::SetPlayerColor(const FLinearColor& Color)
+{
+    const auto MaterialInst = GetMesh()->CreateAndSetMaterialInstanceDynamic(0);
+    if (!MaterialInst) return;
 
-
-
-
-
-
-
+    MaterialInst->SetVectorParameterValue(MaterialColorName, Color);
+}
--- Source/MyProject/Public/TestCharacter.h
+++ Source/MyProject/Public/TestCharacter.h
@@ -1,17 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Shoot Them Up Game, All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/DamageType.h"
 #include "GameFramework/Character.h"
 #include "TestCharacter.generated.h"
 
-
-class UCameraComponent;
-class USpringArmComponent;
 class USTUHealthComponent;
-class UTextRenderComponent;
 class USTUWeaponComponent;
 
 UCLASS()
@@ -20,82 +15,47 @@ class MYPROJECT_API ATestCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ATestCharacter(const FObjectInitializer& ObjInit);
-	
 
 protected:
-	// Called when the game starts or when spawned
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USTUHealthComponent* HealthComponent;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, category = "Components")
-	UCameraComponent* CameraComponent;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, category = "Components")
-	USpringArmComponent* SpringArmComponent;
-
-
-	UPROPERTY(VisibleAnywhere,BlueprintReadWrite, category = "Components")
-	USTUHealthComponent* STUHealthComponent;
-
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Health")
-	UTextRenderComponent* TextRenderComponent;
-
-	UPROPERTY(VisibleAnywhere,BlueprintReadWrite, category = "Components")
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	USTUWeaponComponent* WeaponComponent;
-	
-	UPROPERTY(EditDefaultsOnly, category = "Animations")
+
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
 	UAnimMontage* DeathAnimMontage;
 
-	UPROPERTY(EditDefaultsOnly, category = "Damage")
-	FVector2D VelocityLandedDamage = FVector2D(900.0f, 1200.0f);
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float LifeSpanOnDeath = 5.0f;
 
-	UPROPERTY(EditDefaultsOnly, category = "Damage")
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	FVector2D LandedDamageVelocity = FVector2D(900.0f, 1200.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FVector2D LandedDamage = FVector2D(10.0f, 100.0f);
-	
-	UPROPERTY(EditDefaultsOnly, category = "Material")
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FName MaterialColorName = "Paint Color";
-	
-	
+
 	virtual void BeginPlay() override;
 	virtual void OnDeath();
-	
-public:	
-	// Called every frame
+
+public:
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
 	UFUNCTION(BlueprintCallable, Category = "Movement")
-	bool IsRunning() const;
+	virtual bool IsRunning() const;
 
 	UFUNCTION(BlueprintCallable, Category = "Movement")
-	float GetActorDirection() const;
-	
-	
-	void SetPlayerColor(const FLinearColor Color);
-	
+	float GetMovementDirection() const;
+
+	void SetPlayerColor(const FLinearColor& Color);
 
 private:
-	//Running
-	bool WantsToRun = false;
-	bool IsMoving = false;
-
-	//Moving
-	void MoveForward(float Value);
-	void MoveRight(float Value);
-	void OnFastRunning();
-	void OnStopRunning();
-
-	//camera moving
-	void LookUp(float Value);
-	void TurnRight(float Value);
-
-	//Death and Damaging
 	void OnHealthChanged(float Health, float HealthDelta);
 
 	UFUNCTION()
 	void OnGroundLanded(const FHitResult& Hit);
-	
 };
